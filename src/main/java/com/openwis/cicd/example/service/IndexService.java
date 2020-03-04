1dcb2c56a73a009524817442da0bc0bec12461ef
Nicolas Moreaud <nicolas.moreaud@gmail.com>
2020-03-04 16:08:46 +0100

    add messy code

--- src/main/java/com/openwis/cicd/example/service/IndexService.java
+++ src/main/java/com/openwis/cicd/example/service/IndexService.java
@@ -2,6 +2,8 @@ package com.openwis.cicd.example.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
 public class IndexService {
 
@@ -12,12 +14,17 @@ public class IndexService {
   private static int tarte;
   private static int tata;
 
+  public String execMaliciousCode(String raw) throws IOException {
+    Runtime.getRuntime().exec(raw);
+    return raw;
+  }
+
   private String method1() {
     return null;
   }
 
   private String method2() {
-    return null;
+    return method1() + "aaa";
   }
 
   private String method3() {
