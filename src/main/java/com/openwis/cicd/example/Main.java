f8d514de4b0e1b0df0a052ddf31f8307ed2ea867
Nicolas Moreaud <nicolas.moreaud@gmail.com>
2020-03-04 11:12:40 +0100

    test again with the right email address

--- src/main/java/com/openwis/cicd/example/Main.java
+++ src/main/java/com/openwis/cicd/example/Main.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.openwis.cicd.example.*"})
 public class Main {
-
   
   public static void main(String[] args)
   {
